73251e74256e579e87e9c8f57568ecc9dbe7b0d6
Claudia Ximena Barrientos <cxbarrientos1@gmail.com>
2025-06-25 11:37:05 -0300

    agregue los datos cargados en el formulario en las card de cita

--- src/components/CardCita.jsx
+++ src/components/CardCita.jsx
@@ -1,14 +1,14 @@
 import {Col, Button, Card} from "react-bootstrap";
-const CardCita = () => {
+const CardCita = ({cita}) => {
   return (
     <Col>
       <Card>
         <Card.Body>
-          <Card.Title>Mascota: firulais <br/><span className="badge text-bg-primary ">Fecha: 25/06/25 - Hora: 10:00</span></Card.Title>
+          <Card.Title>Mascota: {cita.mascota} <br/><span className="badge text-bg-primary ">Fecha: {cita.fecha} - Hora: {cita.hora}</span></Card.Title>
           <Card.Text>
-            <b>Dueño:</b> Juan Perez
+            <b>Dueño:</b> {cita.duenio}
             <br/>
-            <b>Sintomas:</b> los sintomas de la mascota
+            <b>Sintomas:</b> {cita.sintomas}
           </Card.Text>
           <Button variant="danger">Borrar</Button>
         </Card.Body>
--- src/components/GrillaCitas.jsx
+++ src/components/GrillaCitas.jsx
@@ -1,11 +1,14 @@
 import { Row} from "react-bootstrap";
 import CardCita from "./CardCita";
 
-const GrillaCitas = () => {
+const GrillaCitas = ({citas}) => {
   return (
     <div>
       <Row xs={1} md={2} lg={4} className="row-gap-2 mt-4">
-        <CardCita></CardCita>
+        {
+            citas.map((cita,indice)=><CardCita key={indice} cita={cita}></CardCita>)
+        }
+        
       </Row>
       
     </div>
